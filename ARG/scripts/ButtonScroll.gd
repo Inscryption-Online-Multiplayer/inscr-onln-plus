6aa5a0590a6afae5c6d234e639692960a5938f77
Mouthless Stoat <nphuyanh@gmail.com>
2024-01-21 00:08:48 -0400

    Warning go bye bye

--- ARG/scripts/ButtonScroll.gd
+++ ARG/scripts/ButtonScroll.gd
@@ -2,8 +2,8 @@ extends ScrollContainer
 
 
 func _on_RightButton_pressed():
-	scroll_vertical += rect_size.y 
+	scroll_vertical += rect_size.y as int
 
 
 func _on_LeftButton_pressed():
-	scroll_vertical -= rect_size.y
+	scroll_vertical -= rect_size.y as int
